7d5f311f246976887e93da5a04af5bad9fd1b76e
AndreasBesar29 <161665251+AndreasBesar29@users.noreply.github.com>
2024-05-07 07:59:59 +0700

    Update Unguided2.cpp

--- Modul 6 STACK/Unguided.cpp/Unguided2.cpp
+++ Modul 6 STACK/Unguided.cpp/Unguided2.cpp
@@ -1,3 +1,38 @@
+// Andreas Besar Wibowo
+// 2311102198 / IF-11-E
+
 #include <iostream>
+#include <stack>
+#include <string>
 
 using namespace std;
+
+string balikKalimat(string Kalimat) {
+    stack<char> stackChar;
+
+    for (char a : Kalimat) {
+        stackChar.push(a);
+    }
+
+    string pembalikanKalimat = "";
+
+    while (!stackChar.empty()) {
+        pembalikanKalimat += stackChar.top();
+        stackChar.pop();
+    }
+
+    return pembalikanKalimat;
+}
+
+int main() {
+    string kalimat;
+
+    cout << "Masukkan kalimat: ";
+    getline(cin, kalimat);
+
+    string pembalikanKalimat = balikKalimat(kalimat);
+
+    cout << "Hasil kalimat setelah dibalik: " << pembalikanKalimat << endl;
+
+    return 0;
+}
