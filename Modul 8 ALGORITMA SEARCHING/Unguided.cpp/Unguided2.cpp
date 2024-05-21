7c626c338696380a6bd33900d13df77d31230228
AndreasBesar29 <161665251+AndreasBesar29@users.noreply.github.com>
2024-05-22 06:51:24 +0700

    Update Unguided2.cpp

--- Modul 8 ALGORITMA SEARCHING/Unguided.cpp/Unguided2.cpp
+++ Modul 8 ALGORITMA SEARCHING/Unguided.cpp/Unguided2.cpp
@@ -1,3 +1,31 @@
+// Andreas Besar Wibowo
+// 2311102198 / S1 IF-11-E
+
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+bool isVowel(char j) {
+    j = tolower(j);
+    return (j == 'a' || j == 'e' || j == 'i' || j == 'o' || j == 'u');
+}
+
+int main() {
+    string kalimat;
+    int jumlahHuruf_Vokal = 0;
+
+      cout << "Tuliskan Sebuah Kalimat: ";
+    getline(cin, kalimat);
+
+    for (char j : kalimat) {
+        if (isVowel(j)) {
+            jumlahHuruf_Vokal++;
+        }
+    }
+
+   
+    cout << "Jumlah huruf vokal yang terdapat di dalam kalimat yaitu : " << jumlahHuruf_Vokal << endl;
+
+    return 0;
+}
