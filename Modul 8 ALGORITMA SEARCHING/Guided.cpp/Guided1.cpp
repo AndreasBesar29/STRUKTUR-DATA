a921bf960f190142518403e1a1cf168be0ad147f
AndreasBesar29 <161665251+AndreasBesar29@users.noreply.github.com>
2024-05-22 10:18:38 +0700

    Update Guided1.cpp

--- Modul 8 ALGORITMA SEARCHING/Guided.cpp/Guided1.cpp
+++ Modul 8 ALGORITMA SEARCHING/Guided.cpp/Guided1.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 
 using namespace std;
+
+int arr[] = {1,4,2,5,6,7,8,9,10,15,14};
+int cari = 15;
+int i = 0;
+int panjangArray = sizeof(arr)/sizeof(arr[0]);
+bool ketemu = false;
+
+int main () {
+    for (i = 0; i < panjangArray; i++){
+        if (arr[i] == cari) {
+            ketemu = true;
+            break;
+        } else {
+            i++;
+        }
+    }
+    cout << "panjang array: " << panjangArray << endl;
+    cout << cari << " terdapat pada indeks ke- " << arr[i];
+}
